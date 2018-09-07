a84e8e28a7762d3e33bcb019a5a2d6a11ec3a86e
Mike Chowla <mike.chowla@pubmatic.com>
2018-09-07 12:04:07 -0700

    Add PubMatic's new header bidding endpoint

--- src/js/definitions/calls.js
+++ src/js/definitions/calls.js
@@ -822,6 +822,7 @@ export default [
         listen:     [
             '.pubmatic.com/AdServer/AdCallAggregator',
             '.pubmatic.com/AdServer/AdServerServlet',
+            '.pubmatic.com/translator',
         ],
     },
     {
